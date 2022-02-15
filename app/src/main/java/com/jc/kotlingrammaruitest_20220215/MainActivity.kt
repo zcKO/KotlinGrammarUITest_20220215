4619af3a585034d05f625b35e249bbed37b67ff1
JaeCheon <studyJcHub@gmail.com>
2022-02-15 15:50:22 +0900

    첫 번째 문제

--- app/src/main/java/com/jc/kotlingrammaruitest_20220215/MainActivity.kt
+++ app/src/main/java/com/jc/kotlingrammaruitest_20220215/MainActivity.kt
@@ -2,10 +2,31 @@ package com.jc.kotlingrammaruitest_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginButton.setOnClickListener {
+
+            // admin info
+            val adminId = "admin"
+            val adminPassword = "asdf"
+
+            // input info
+            val id = idEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (id == adminId && password == adminPassword) {
+                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 }
